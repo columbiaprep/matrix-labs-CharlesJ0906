85b0b4cc57e65fb770ac62011d300a4d70fcfe5e
cjinn25 <cjinn25@cgps.org>
2024-02-06 12:30:09 -0500

    Tuesday's work

---
+++ main.java
@@ -0,0 +1,16 @@
+public class main{
+    public static int[][] scale(int[][] input, int scalingfactor){
+        // create a new 2d array called w/ row/col count the same
+        int[][] output = new int[input.length][input[0].length]{
+
+
+
+        }
+        //iterate throught th 2d array input
+        // for each value,multiply it by the scaling factor
+        // place that value in the output array in the same spot
+        // return new array output
+
+
+    }
+}
